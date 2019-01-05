a6809cb479306605ec4f375245e97ba2e5ec9a5d
suvankar22 <suvankar22@gmail.com>
2019-01-05 22:07:57 +0530

    first commit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ let token = ""
 // Facebook 
 
 app.get('/webhook/', function(req, res) {
-	if (req.query['hub.verify_token'] === "blondiebytes") {
+	if (req.query['hub.verify_token'] === "jawan jyoti") {
 		res.send(req.query['hub.challenge'])
 	}
 	res.send("Wrong token");
