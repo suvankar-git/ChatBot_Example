1ff366779c20adb59f6b2e91f8ca134d0ee44fac
suvankar22 <suvankar22@gmail.com>
2019-01-05 21:16:29 +0530

    Chatb\Bot AAdded

--- index.js
+++ index.js
@@ -1,32 +1,66 @@
-"use strict";
+'use strict'
 
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
 
-const restService = express();
+const app = express()
 
-restService.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
+app.set('port', (process.env.PORT || 5000))
 
-restService.use(bodyParser.json());
+// Allows us to process the data
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
-restService.post("/echo", function(req, res) {
-  var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
-      : "Seems like some problem. Speak again.";
-  return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
-  });
-});
+// ROUTES
 
-restService.listen(process.env.PORT || 8000, function() {
-  console.log("Server up and listening");
-});
+app.get('/', function(req, res) {
+	res.send("Hi I am a chatbot")
+})
+
+let token = ""
+
+// Facebook 
+
+app.get('/webhook/', function(req, res) {
+	if (req.query['hub.verify_token'] === "blondiebytes") {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send("Wrong token");
+})
+
+/*app.post('/webhook/', function(req, res) {
+	let messaging_events = req.body.entry[0].messaging
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = messaging_events[i]
+		let sender = event.sender.id
+		if (event.message && event.message.text) {
+			let text = event.message.text
+			sendText(sender, "Text echo: " + text.substring(0, 100))
+		}
+	}
+	res.sendStatus(200)
+})
+
+function sendText(sender, text) {
+	let messageData = {text: text}
+	request({
+		url: "https://graph.facebook.com/v2.6/me/messages",
+		qs : {access_token: token},
+		method: "POST",
+		json: {
+			recipient: {id: sender},
+			message : messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log("sending error")
+		} else if (response.body.error) {
+			console.log("response body error")
+		}
+	})
+}*/
+
+app.listen(app.get('port'), function() {
+	console.log("running: port")
+})
