6512059a95c0ba715dfb24a5c1b30688443b2e7b
suvankar22 <suvankar22@gmail.com>
2019-01-05 23:00:57 +0530

    first commit

--- index.js
+++ index.js
@@ -31,20 +31,20 @@ app.get('/webhook/', function(req, res) {
 })
 
 app.post('/webhook/', function(req, res) {
-	let messaging_events = req.body.entry[0].messaging
+	let messaging_events = req.body.entry[0].messaging;
 	for (let i = 0; i < messaging_events.length; i++) {
-		let event = messaging_events[i]
-		let sender = event.sender.id
+		let event = messaging_events[i];
+		let sender = event.sender.id;
 		if (event.message && event.message.text) {
-			let text = event.message.text
-			sendText(sender, "Text echo: " + text.substring(0, 100))
+			let text = event.message.text;
+			sendText(sender, "Text echo: " + text.substring(0, 100));
 		}
 	}
 	res.sendStatus(200)
 })
 
 function sendText(sender, text) {
-	let messageData = {text: text}
+	let messageData = {text: text};
 	request({
 		url: "https://graph.facebook.com/v2.6/me/messages",
 		qs : {access_token: token},
